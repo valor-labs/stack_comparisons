8dd7d4b1af20a0066b8057549fc1853fe7162507
zackarychapple <zackarychapple30@gmail.com>
2022-04-25 11:09:54 -0400

    feat(react): updating react todo app to use hooks

--- apps/react-todo/src/app/app.tsx
+++ apps/react-todo/src/app/app.tsx
@@ -1,68 +1,68 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 
-import { Task } from "./models/task";
-import { NewTaskForm } from "./components/NewTaskForm";
-import { TasksList } from "./components/TasksList";
+import {Task} from "./models/task";
 
-interface State {
-  newTask: Task;
-  tasks: Task[];
-}
+function App() {
+  const [todos, setTodos] = useState([{id: 0, name: 'Starting Item'}] as Array<Task>);
 
-class App extends Component<{}, State> {
-  override state = {
-    newTask: {
-      id: 1,
-      name: ""
-    },
-    tasks: [{
-      id:0,
-      name:'Starting Item'
-    }]
-  };
-
-  override render() {
-    return (
-      <div>
-        <h2>Hello React TS!</h2>
-        <NewTaskForm
-          task={this.state.newTask}
-          onAdd={this.addTask}
-          onChange={this.handleTaskChange}
-        />
-        <TasksList tasks={this.state.tasks} onDelete={this.deleteTask} />
-      </div>
-    );
+  const addTodo = (name: string): void => {
+    const newTodos: any = [...todos, {name}];
+    setTodos(newTodos);
   }
 
-  private addTask = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const removeTodo = (index: number): void => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos);
+  }
 
-    this.setState(previousState => ({
-      newTask: {
-        id: previousState.newTask.id + 1,
-        name: ""
-      },
-      tasks: [...previousState.tasks, previousState.newTask]
-    }));
-  };
-
-  private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      newTask: {
-        ...this.state.newTask,
-        name: event.target.value
-      }
-    });
-  };
-
-  private deleteTask = (taskToDelete: Task) => {
-    this.setState(previousState => ({
-      tasks: [
-        ...previousState.tasks.filter(task => task.id !== taskToDelete.id)
-      ]
-    }));
-  };
+  return (
+    <div>
+      <h2>Hello React TS!</h2>
+      <TodoForm addTodo={addTodo}/>
+      <ul>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
+
+function Todo({todo, index, removeTodo}: any) {
+  return (
+    <li>
+      {todo.name}
+      <button onClick={() => removeTodo(index)}>Delete</button>
+    </li>
+  )
+}
+
+function TodoForm({addTodo}: any) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
