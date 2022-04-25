9a37b8840e58a887b430f6d9795f93f13617912f
zackarychapple <zackarychapple30@gmail.com>
2022-04-25 15:00:50 -0400

    feat(qwik): adding qwik todo

--- apps/qwik-todo/src/app/main.tsx
+++ apps/qwik-todo/src/app/main.tsx
@@ -1,33 +1,43 @@
 import { component$, useStore } from '@builder.io/qwik';
-import { Footer } from './components/footer/footer';
-import { Header } from './components/header/header';
-import { Body } from './components/body/body';
 import type { Todos } from './state/state';
 
-import './base.css';
-import './index.css';
-
-/**
- * Overall application component.
- *
- * This component is static (meaning it will never change). Because of this
- * Qwik knows that it should never need to be rerendered, and its code will never
- * download to the client.
- */
 export const Main = component$(() => {
+  const state = useStore({ text: '' });
   const todos = useStore<Todos>({
-    filter: 'all',
     items: [
-      { completed: false, title: 'Read Qwik docs' },
-      { completed: false, title: 'Build HelloWorld' },
-      { completed: false, title: 'Profit' },
+      { index: 0, name: 'Starting Item' }
     ],
   });
   return (
-    <section class="todoapp">
-      <Header todos={todos} />
-      <Body todos={todos} />
-      <Footer todos={todos} />
-    </section>
+    <div>
+      <h2>Hello QWIK</h2>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus
+        value={state.text}
+        onKeyup$={(event: any) => {
+          const inputValue = (event.target as HTMLInputElement).value;
+          state.text = inputValue;
+          if (event.key === 'Enter' && inputValue) {
+            todos.items.push({
+              index:1,
+              name: state.text,
+            });
+            state.text = '';
+          }
+        }}
+      />
+      <ul className="todo-list">
+        {todos.items.map((todo) => (
+          <li>{todo.name}<button
+            onClick$={() => {
+              const todoItem = todo;
+              todos.items = todos.items.filter((i) => i != todoItem);
+            }}
+          >Delete</button></li>
+        ))}
+      </ul>
+    </div>
   );
 });
--- apps/qwik-todo/src/app/state/state.ts
+++ apps/qwik-todo/src/app/state/state.ts
@@ -3,21 +3,10 @@
 ////////////////////////////////////////////////////////////////////////
 
 export interface TodoItem {
-  completed: boolean;
-  title: string;
+  index: number;
+  name: string;
 }
 
 export interface Todos {
-  filter: FilterStates;
   items: TodoItem[];
 }
-
-export type FilterStates = 'all' | 'active' | 'completed';
-
-export const FilterStates: FilterStates[] = ['all', 'active', 'completed'];
-
-export const FILTERS = {
-  all: () => true,
-  active: (i: TodoItem) => i.completed == false,
-  completed: (i: TodoItem) => i.completed == true,
-};
