4f50380a8a4c58133cc877e3cb149a588b36f800
zackarychapple <zackarychapple30@gmail.com>
2022-04-25 09:51:20 -0400

    feat(react): adding basic react todo app

--- apps/react-todo/src/app/app.tsx
+++ apps/react-todo/src/app/app.tsx
@@ -1,14 +1,68 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import React, { Component } from "react";
 
-export function App() {
-  return (
-    <>
-      <NxWelcome title="react-todo" />
-      <div />
-    </>
-  );
+import { Task } from "./models/task";
+import { NewTaskForm } from "./components/NewTaskForm";
+import { TasksList } from "./components/TasksList";
+
+interface State {
+  newTask: Task;
+  tasks: Task[];
+}
+
+class App extends Component<{}, State> {
+  override state = {
+    newTask: {
+      id: 1,
+      name: ""
+    },
+    tasks: [{
+      id:0,
+      name:'Starting Item'
+    }]
+  };
+
+  override render() {
+    return (
+      <div>
+        <h2>Hello React TS!</h2>
+        <NewTaskForm
+          task={this.state.newTask}
+          onAdd={this.addTask}
+          onChange={this.handleTaskChange}
+        />
+        <TasksList tasks={this.state.tasks} onDelete={this.deleteTask} />
+      </div>
+    );
+  }
+
+  private addTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    this.setState(previousState => ({
+      newTask: {
+        id: previousState.newTask.id + 1,
+        name: ""
+      },
+      tasks: [...previousState.tasks, previousState.newTask]
+    }));
+  };
+
+  private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newTask: {
+        ...this.state.newTask,
+        name: event.target.value
+      }
+    });
+  };
+
+  private deleteTask = (taskToDelete: Task) => {
+    this.setState(previousState => ({
+      tasks: [
+        ...previousState.tasks.filter(task => task.id !== taskToDelete.id)
+      ]
+    }));
+  };
 }
 
 export default App;
---
+++ apps/react-todo/src/app/components/TasksList.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+
+import { Task } from "../models/task";
+import { TaskListItem } from "./TasksListItem";
+
+interface Props {
+  tasks: Task[];
+  onDelete: (task: Task) => void;
+}
+
+export const TasksList: FunctionComponent<Props> = ({ tasks, onDelete }) => (
+  <ul>
+    {tasks.map(task => (
+      <TaskListItem task={task} onDelete={onDelete} />
+    ))}
+  </ul>
+);
